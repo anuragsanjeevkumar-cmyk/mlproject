3440a8bbfee64daf080db5f4288f1b469efad9bb
anuragsanjeevkumar-cmyk <anuragsanjeevkumar@gmail.com>
2026-06-11 12:43:25 +0530

    Data transformation is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
 
 
 @dataclass
@@ -23,14 +25,17 @@ class DataIngestion:
         logging.info("Entered the data ingestion component")
 
         try:
+            # Read dataset
             df = pd.read_csv(r"notebook\data\stud.csv")
             logging.info("Read the dataset as DataFrame")
 
+            # Create artifacts folder
             os.makedirs(
                 os.path.dirname(self.ingestion_config.train_data_path),
                 exist_ok=True
             )
 
+            # Save raw data
             df.to_csv(
                 self.ingestion_config.raw_data_path,
                 index=False,
@@ -39,18 +44,21 @@ class DataIngestion:
 
             logging.info("Train test split initiated")
 
+            # Split dataset
             train_set, test_set = train_test_split(
                 df,
                 test_size=0.2,
                 random_state=42
             )
 
+            # Save train data
             train_set.to_csv(
                 self.ingestion_config.train_data_path,
                 index=False,
                 header=True
             )
 
+            # Save test data
             test_set.to_csv(
                 self.ingestion_config.test_data_path,
                 index=False,
@@ -66,7 +74,17 @@ class DataIngestion:
 
         except Exception as e:
             raise CustomException(e, sys)
-        
-if __name__=="__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
+
+
+if __name__ == "__main__":
+
+    obj = DataIngestion()
+
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    data_transformation.initiate_data_transformation(
+        train_data,
+        test_data
+    )
